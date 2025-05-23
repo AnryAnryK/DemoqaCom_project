501171a9f24d081aeec867b784ac4da84a9920a1
AnryAnryK <andkay@yandex.ru>
2025-05-23 15:30:49 +0300

    2025.05.23 - в пакете DemoQaPageObject.BrowserWindows - void TestBrowserWindowsNewWindowMessageSuccessful()  - сделал проверку окна через Alert, но продолжает не работать

--- src/test/java/SelenideTestsPageObject/DemoQaPageObject/BrowserWindows/BrowserWindows.java
+++ src/test/java/SelenideTestsPageObject/DemoQaPageObject/BrowserWindows/BrowserWindows.java
@@ -7,11 +7,9 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.SearchContext;
 
 import java.io.IOException;
 
-import static com.codeborne.selenide.Condition.tagName;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
@@ -63,40 +61,33 @@ public class BrowserWindows {
 	}
 
 	@Test
-	public void TestBrowserWindowsNewWindowMessageSuccessful () throws IOException {
+	public void TestBrowserWindowsNewWindowMessageSuccessful() throws IOException {
 		step("1 Шаг: зайти на нужную страницу https://demoqa.com/browser-windows", () ->
 		{
 			open(websiteWebTables);
 		});
 
-		step("2 Шаг: нажать на кнопку 'New Window' ", () ->
+		step("2 Шаг: нажать на кнопку 'New Window Message' ", () ->
 				$x(".//*[@id='messageWindowButton']").click());
 
+		step("3 Шаг: проверить, что открылось новое окно (перейти в него) ", () ->
+				Selenide.switchTo().window(1));
 
-		$(By.tagName("body").findElement((SearchContext) tagName("body")));
-//				.shouldHave(text("Knowledge"));
 
-////		step("3 Шаг: проверить, что открылось новое окно (перейти в него) ", () ->
-//		Alert alert = switchTo().alert();
-//		String text = alert.getText();
-//		System.out.println(text);
+		step("4 Шаг: проверить наличие текста 'Knowledge increases by sharing but not by saving. Please share this website with your friends and in your organization.'", () ->
+				$(By.xpath("//body"))); //работает, но даже с текстом, которого в окне about:blank нет ! т.е. НЕ успешный тест
+
+//		step("3 Шаг: проверить, что открылось новое окно (перейти в него) ", () -> // падает на этом шаге, пишет 'Alert not found'
+//		{
+//			Alert jsal = switchTo().alert();
+//		String jsal_text = jsal.getText();
+//		System.out.println(jsal_text);
 //
-//		Assertions.assertEquals(text, "Knowledge increases by sharing but not by saving. Please share this website with your friends and in your organization.");
+//		Assertions.assertEquals("Knowledge increases by sharing but not by saving. Please share this website with your friends and in your organization.", jsal_text);
+//		jsal.accept();
+//		});
 
-//				Selenide.switchTo().window(1).byTagAndText("body","Knowledge").hover();
-//				Selenide.switchTo().window(1)).findElements(byTagAndText("body","Knowledge"));
-//				findElement(withText(("Knowledge")));
-//				Selenide.switchTo().window(1)).findElement(withText(("Knowledge")));
 
-//		step("4 Шаг: проверить наличие текста 'Knowledge increases by sharing but not by saving. Please share this website with your friends and in your organization.'", () ->
-//				Selenide.selectByV($x("/html/body/text()")));
-
-//		$(withText("Knowledge")).shouldBe(visible);
-//		step("4 Шаг: проверить наличие текста 'Knowledge increases by sharing but not by saving. Please share this website with your friends and in your organization.'", () ->
-//				Selenide.switchTo().frame($(byTagAndText("body","Knowledge")).shouldHave(text("Knowledge"))));
-
-//		step("4 Шаг: проверить наличие текста 'Knowledge increases by sharing but not by saving. Please share this website with your friends and in your organization.'", () ->
-//				Selenide.switchTo().frame($x("/html/body/text()")));
 
 //			step("4 Шаг: проверить наличие текста 'Knowledge increases by sharing but not by saving. Please share this website with your friends and in your organization.'", () ->
 //					$(byTagAndText("body","Knowledge increases by sharing but not by saving. Please share this website with your friends and in your organization.")));  //этот вариант - проходит, даже если совсем убрать значение аттрибута (т.е. что-то тут некорректно !)
