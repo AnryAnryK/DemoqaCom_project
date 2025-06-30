e92d1f2f3bf1061f0f8193576ee85f1fab25c419
AnryAnryK <andkay@yandex.ru>
2025-06-30 13:18:17 +0300

    2025.06.30 - довёл до ума package JavaCore.lesson6 _ public class Student1  (в Конструктор Student1(int a1, int b1) { 		this(a1, b1, 0, 0, 0); 	}
    	вставил this из Конструктора
    
    	Student1(int a4, int b4, int c4, int d4, int e4) {
        		zero = a4;
        		one = b4;
        		two = c4;
        		three = d4;
        		four = e4;
        	}
    
        	и вывел на экран:
    
        	Student1 student1 = new Student1(1, 10);
            		System.out.print("Результат возврата сложения по методу summ из двух чисел: ");
            		student1.summ(20, 30);

--- src/test/java/JavaCore/lesson6/Student1.java
+++ src/test/java/JavaCore/lesson6/Student1.java
@@ -1,97 +1,91 @@
 package JavaCore.lesson6;
 
+
 public class Student1 {
 
-		int zero;
-		int one;
-		int two;
-		int three;
-		int four;
-		int id;
-		String name;
-		String surname;
-		String course;
+	int zero;
+	int one;
+	int two;
+	int three;
+	int four;
+	int id;
+	String name;
+	String surname;
+	String course;
 
 
-		Student1(int a0) {
-			zero = a0;
-		}
-
-		Student1(int a1, int b1) {
-			this(a1);
-			one = b1;
-		}
-
-		Student1(int a2, int b2, int c2) {
-			this(a2, b2);
-			two = c2;
-		}
-
-		Student1(int a3, int b3, int c3, int d3) {
-			this(a3, b3, c3);
-			three = d3;
-		}
-
-		Student1(int a4, int b4, int c4, int d4, int e4) {
-			this(a4, b4, c4, d4);
-			four = e4;
-		}
-
-		Student1(int id, String name, String surname, String course) {
-			this.id = id;
-			this.name = name;
-			this.surname = surname;
-			this.course = course;
-		}
-
-		Student1(String name, String surname) {
-			this.name = name;
-			this.surname = surname;
-		}
-
-		Student1() {
-		}
+	Student1(int a1, int b1) {
+		this(a1, b1, 0, 0, 0);
+	}
 
 
+	Student1(int a4, int b4, int c4, int d4, int e4) {
+		zero = a4;
+		one = b4;
+		two = c4;
+		three = d4;
+		four = e4;
+	}
 
-		void summ(int a) {
-			zero = a;
-			System.out.println(a);
-		}
+//		Student1(int id, String name, String surname, String course) {
+//			this.id = id;
+//			this.name = name;
+//			this.surname = surname;
+//			this.course = course;
+//		}
+//
+//		Student1(String name, String surname) {
+//			this.name = name;
+//			this.surname = surname;
+//		}
 
-		void summ(int a, int b) {
-			zero = a;
-			one = b;
-			System.out.println(a + b);
-		}
+//		Student1() {
+//		}
 
-		void summ(int a, int b, int c) {
-			zero = a;
-			one = b;
-			two = c;
-			System.out.println(a + b + c);
-		}
 
-		void summ(int a, int b, int c, int d) {
-			zero = a;
-			one = b;
-			two = c;
-			three = d;
-			System.out.println(a + b + c + d);
-		}
+//	void summ(int a) {
+//		zero = a;
+//		System.out.println(a);
+//	}
 
-		void summ(int a, int b, int c, int d, int e) {
-			zero = a;
-			one = b;
-			two = c;
-			three = d;
-			four = e;
-			System.out.println(a + b + c + d + e);
-		}
+
+	void summ(int a, int b) {
+		zero = a;
+		one = b;
+		System.out.println(a + b);
+
+	}
+
+	void summ(int a, int b, int c) {
+		zero = a;
+		one = b;
+		two = c;
+		System.out.println(a + b + c);
+	}
+
+	void summ(int a, int b, int c, int d) {
+		zero = a;
+		one = b;
+		two = c;
+		three = d;
+		System.out.println(a + b + c + d);
+	}
+
+	void summ(int a, int b, int c, int d, int e) {
+		zero = a;
+		one = b;
+		two = c;
+		three = d;
+		four = e;
+		System.out.println(a + b + c + d + e);
+	}
 
 	public static void main(String[] args) {
-		Student1 student1 = new Student1();
+		Student1 student1 = new Student1(1, 10);
+		System.out.print("Результат возврата сложения по методу summ из двух чисел: ");
+		student1.summ(20, 30);
 
 	}
-	}
+}
+
 
